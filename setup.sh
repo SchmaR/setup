ba8493973853c0f10155da7beb55afe084523a1e
Rudolf Schneider <post@rudolfschneider.de>
2014-10-28 02:34:57 +0100

    init and version 1.0

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "please make sure xcode ist installed right with : xcode-select --install "
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+echo " "
+echo "########## installing standart components ... ################"
+echo " "
+brew install git
+brew install python3
+brew cask install java
+brew cask install google-chrome
+brew cask install firefox
+brew cask install adium
+brew cask install skype
+brew cask install gpgtools
+brew cask install eclipse-jee
+brew cask install android-studio-bundle
+brew cask install xampp
+brew cask install iterm2
+brew cask install sourcetree
+brew cask install filezilla
+brew cask install lyx
+brew cask install bibdesk
+brew cask install textwrangler
+brew cask install openoffice
+brew cask install jdownloader
+brew cask install steam
+brew cask install vlc
+brew cask install flash-player
+
+
