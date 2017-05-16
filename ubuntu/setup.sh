2aaf592c0f2c4e8b79b49315eebfb3a69ff0980a
rschneider <rudolf.schneider@beuth-hochschule.de>
2017-05-16 15:37:31 +0200

    added more software

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -10,7 +10,29 @@ sudo apt-get install -y filezilla
 sudo apt-get install -y vlc
 sudo apt-get install -y wireshark
 sudo apt-get install -y virtualbox
-sudo apt-get install -y qpdfview
+sudo apt-get install -y okular
+sudo apt-get install -y texmaker
+sudo apt-get install -y gimp
+sudo apt-get install -y gnupg
+sudo apt-get install -y gpgv2
+sudo apt-get install -y hunspell-de-de
+sudo apt-get install -y hunspell-en-ca
+sudo apt-get install -y kdenlive
+sudo apt-get install -y keepassx
+sudo apt-get install -y nano
+sudo apt-get install -y pandoc
+sudo apt-get install -y powertop
+sudo apt-get install -y psensor
+sudo apt-get install -y remarkable
+sudo apt-get install -y thunderbird
+sudo apt-get install -y wavemon
+sudo apt-get install -y vim
+sudo apt-get install -y zenmap
+sudo apt-get install -y mtr-tiny
+# chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo apt-get update 
+sudo apt-get install -y google-chrome-stable
 # oracle java
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
